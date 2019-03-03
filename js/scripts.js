f91781a75d34e21326623423009c2cf109b1b59b
Sowayi <sapwao@gmail.com>
2019-03-03 05:06:09 -0500

    Add function to display roll value of the dice on the roll board

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var scores1=[]
+$(document).ready(function(){
+    $("#player1 #roll1").click(function(){
+        //var roll = Math.floor(Math.random() * 6) + 1)
+        var roll=$("#scoreboard1 h1").text(Math.floor(Math.random() * 6) + 1)
+        scores1.push(roll);
+    })
+})
+$(document).ready(function(){
+    $("").click(function(){
+        scores1.each(function(){
+            var total = scores1[0]
+        })
+    })
+})
+
