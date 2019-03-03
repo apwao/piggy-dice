ac1b617b809bce26cf609d6fa302cda3fbb35999
Sowayi <sapwao@gmail.com>
2019-03-03 05:17:35 -0500

    Add function to display roll value for player2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var scores1=[]
+//
 $(document).ready(function(){
     $("#player1 #roll1").click(function(){
         //var roll = Math.floor(Math.random() * 6) + 1)
@@ -6,6 +6,19 @@ $(document).ready(function(){
         scores1.push(roll);
     })
 })
+$(document).ready(function(){
+    $("#player2 #roll2").click(function(){
+        //var roll = Math.floor(Math.random() * 6) + 1)
+        var roll=$("#scoreboard2 h1").text(Math.floor(Math.random() * 6) + 1)
+        scores1.push(roll);
+    })
+})
+
+
+
+
+
+
 $(document).ready(function(){
     $("").click(function(){
         scores1.each(function(){
