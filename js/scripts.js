c0587ff94522af5e12bfb4e12486e825abaef90b
Sowayi <sapwao@gmail.com>
2019-03-03 07:34:25 -0500

    Add function to place roll values in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,45 @@
-//
+
+var total1=0;
+var total2=0;
+var scores1=[]
+var scores2 =[]
+var getScore1 =scores1.forEach(function(score){
+    total1 += score
+})
+//User Logic
+//Display roll value on the dice for player1
 $(document).ready(function(){
     $("#player1 #roll1").click(function(){
         //var roll = Math.floor(Math.random() * 6) + 1)
-        var roll=$("#scoreboard1 h1").text(Math.floor(Math.random() * 6) + 1)
-        scores1.push(roll);
+        //Displays roll value on roll board
+        var rollPlayer1 =$("#scoreboard1 h1").text(Math.floor(Math.random() * 6) + 1)
+        //Retrieves the value on the dice
+        var rollValue = $("#scoreboard h1").text()
+        scores1.push(rollValue);
+        
     })
 })
+//Display roll value on dice for player2
 $(document).ready(function(){
     $("#player2 #roll2").click(function(){
         //var roll = Math.floor(Math.random() * 6) + 1)
-        var roll=$("#scoreboard2 h1").text(Math.floor(Math.random() * 6) + 1)
-        scores1.push(roll);
+        var rollPlayer2=$("#scoreboard2 h1").text(Math.floor(Math.random() * 6) + 1)
+        scores2.push(rollPlayer2);
     })
 })
+
+// Implement hold functionality for player1
+//  $(document.ready(function(){
+//     $("#player1 #hold1").click(function(){
+//         var holdPlayer1=$("#scoreboard1 h1").text() 
+//     })
+
+//         var holdPlayer1= $("#scoreboard1 h1").text()
+//          alert("yipee ki yaay");
+//      })
+//  })
+
+// //Implement hold value for player2
 
 
 
