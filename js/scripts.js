3708e908b1e1fb0d608ef2850d31bdd9a796b540
Sowayi <sapwao@gmail.com>
2019-03-05 03:35:58 -0500

    Add construtor functions to business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,39 @@
 // Business logic
+ function Player(name){
+    this.name = name;
+}
+Player.prototype.view_player = function(){
+    this.name
+}
 
-var total1=0;
-var total2=0;
-var scores1=[]
-var scores2 =[]
-var getScore1 =scores1.forEach(function(score1){
-    total1 += score1
-})
-
-
-
+function Roll(rollValue){
+    this.rollValue = rollValue;
+    this.rollNumbers =[];
+    }
+Roll.prototype.get_roll = function(){
+    this.rollValue = Math.floor(Math.random() * 6) + 1;
+    }
+Roll.prototype.collect_rolls = function(){
+    this.rollNumbers.push(rollValue)
+}
+Roll.prototype.total_score= function(){
+    var total = 0;
+    rollNumbers.each(function(score){
+        total += score
+    })
+}
+Roll.prototype.hold = function(){
+    if (this.rollValue > 1){
+        alert("your turn is over, pass to the next player")
+    }
+}
+ var total1=0;
+ var total2=0;
+ var scores1=[]
+ var scores2 =[]   
+ var getScore1 =scores1.forEach(function(score1){
+     total1 += score1
+ })
 
 //User Logic
 //Display roll value on the dice for player1
@@ -37,15 +61,10 @@ $(document).ready(function(){
     
     })
 })
-
-// // Implement hold functionality for player1
-//  $(document.ready(function(){
-//     $("#player1 #hold1").click(function(){
-//         var holdPlayer1=$("#displayTotal1 #holdScore").text(getScore1()) 
-//     })
-//$(document).ready(function(){
+ // Implement hold functionality for player1
     $("#player1 #hold1").click(function(){
          $("#displayTotal1 #holdScore").text(total1)
+         alert("Your turn is over! pass to the next player")
 })
 
 
@@ -55,12 +74,12 @@ $(document).ready(function(){
 
 
 
-// //         var holdPlayer1= $("#scoreboard1 h1").text()
-// //          alert("yipee ki yaay");
-// //      })
-// //  })
+//          var holdPlayer1= $("#scoreboard1 h1").text()
+//           alert("yipee ki yaay");
+//       })
+//  })
 
-// // //Implement hold value for player2
+// Implement hold value for player2
 
 
 
@@ -68,10 +87,10 @@ $(document).ready(function(){
 
 
 // $(document).ready(function(){
-//     $("").click(function(){
-//         scores1.each(function(){
-//             var total = scores1[0]
-//         })
-//     })
-// })
+//  $("").click(function(){
+//        scores1.each(function(){
+//           var total = scores1[0]
+//          })
+//    })
+//  })
 
